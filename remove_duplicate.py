a1717e26b7319a51f6f5a81292ea5bf141b3c8f5
HaileyDsouza <122242260+HaileyDsouza@users.noreply.github.com>
2024-02-10 20:13:48 -0500

    Update remove_duplicate.py

--- remove_duplicate.py
+++ remove_duplicate.py
@@ -1,12 +1,16 @@
 def removeDuplicates(nums):
     count = 0
-    result = []
     for i in range(0, len(nums)):
         if nums[i] != nums[i-1]:
+            nums[count] = nums[i]
             count += 1
-            result.append(nums[i])
-    return (str(count) + ", " + "nums = " + str(result))
+    return count
 
 nums = [0,0,1,1,1,2,2,3,3,4]
 result = removeDuplicates(nums)
 print(result)
+
+""" 
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
+The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+"""
