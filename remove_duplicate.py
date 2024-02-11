6458bb9baceed530c691d12142288b3a5fb8a3a8
HaileyDsouza <122242260+HaileyDsouza@users.noreply.github.com>
2024-02-10 20:18:31 -0500

    Update remove_duplicate.py

--- remove_duplicate.py
+++ remove_duplicate.py
@@ -1,6 +1,6 @@
 def removeDuplicates(nums):
-    count = 0
-    for i in range(len(nums)):
+    count = 1
+    for i in range(1,len(nums)):
         if nums[i] != nums[i-1]:
             nums[count] = nums[i]
             count += 1
